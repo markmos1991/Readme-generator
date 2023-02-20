0165abc2e29c726fe105cfa921b78210e51d2d6b
markmos1991 <101826190+markmos1991@users.noreply.github.com>
2023-02-20 20:06:04 +0000

    added

--- index.js
+++ index.js
@@ -6,11 +6,50 @@ const generateMarkdown = require("./utils/generateMarkdown");
 // array of questions for user
 const questions = [
 
+
 ];
+
+
+inquirer.prompt([
+  {
+    type: 'input',
+    name: 'Description',
+    message: 'What is your name?',
+  },
+  {
+    type: 'input',
+    name: 'Installation',
+    message: 'How can your application be installed',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'What is the usage of your application',
+  },
+  {
+    type: 'input',
+    name: 'Licsence',
+    message: 'Do you have a licsence for your application?',
+  },
+  {
+    type: 'input',
+    name: 'contribution',
+    message: 'What extneral contributions were made to your application?',
+  },
+  {
+    type: 'input',
+    name: 'contribution',
+    message: 'What extneral contributions were made to your application?',
+  },
+])
+
+
 
 // function to write README file
 function writeToFile(fileName, data) {
 }
+
+
 
 // function to initialize program
 function init() {
@@ -23,13 +62,13 @@ init();
 
 // copied code
 
-// importing the fs file system node module
-const fs = require('fs');
+// // importing the fs file system node module
+// const fs = require('fs');
 
-// log.txt name of the file being processed
-// process argv adds to the file
-// (err) 
-fs.appendFile('readme.md', `${process.argv[2]}\n`, (err) =>
-  // if error logs error, if not error logs message
-  err ? console.error(err) : console.log('Commit logged!')
-);
+// // log.txt name of the file being processed
+// // process argv adds to the file
+// // (err) 
+// fs.appendFile('readme.md', `${process.argv[2]}\n`, (err) =>
+//   // if error logs error, if not error logs message
+//   err ? console.error(err) : console.log('Commit logged!')
+// );
